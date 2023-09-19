0d31e2515d546a6d5dfe9b6fdc9dc9c5837a8d55
Максим Стрельников <maksim_stv@mail.ru>
2023-09-19 11:37:51 +0300

    feat(frontend): added frontend after init

---
+++ frontend/src/router/index.js
@@ -0,0 +1,17 @@
+// Composables
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/layouts/Catalog.vue'),
+    
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router
