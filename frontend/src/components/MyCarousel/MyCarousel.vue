db4ebb0d57fe219947f229fd6b5abfd9ca38d0a6
Стрельников Максим Дмитриевич <mstrelnikov@nic.etu>
2023-09-26 22:00:02 +0300

    feat(frontend): make welcome page

---
+++ frontend/src/components/MyCarousel/MyCarousel.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-carousel cycle hide-delimiters :show-arrows="false">
+    <v-carousel-item v-for="(_, i) in 3" :key="i">
+      <v-sheet height="100%">
+        <img src="src\assets\welcome-img.png" alt="" class="carousel_img" />
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/src/layouts/WelcomePage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="welcome">
+    <my-header />
+    <div class="welcome__carousel">
+        <my-carousel/>
+      </div>
+    <div class="container">
+        <div class="categories">
+            <div class="category">
+                <img src="src\assets\categoryimage1.png" class="category__img "/>
+                <div class="category__title">Смартфоны</div>
+            </div>
+            <div class="category">
+                <img src="src\assets\categoryimage2.png" class="category__img "/>
+                <div class="category__title">Телевизоры</div>
+            </div>
+            <div class="category">
+                <img src="src\assets\categoryimage3.png" class="category__img "/>
+                <div class="category__title">Ноутбуки</div>
+            </div>
+            <div class="category">
+                <img src="src\assets\categoryimage4.png" class="category__img "/>
+                <div class="category__title">Аксессуары</div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyHeader from "@/components/Header/MyHeader.vue";
+import MyCarousel from '../components/MyCarousel/MyCarousel.vue';
+
+export default {
+  components: {
+    MyHeader,
+    MyCarousel,
+  },
+};
+</script>
+
+<style>
+    .welcome__carousel{
+        margin-top: 80px;
+        margin-bottom: 40px;
+    }
+
+    .categories{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .category{
+        background-color: white;
+        border-radius: 20px;
+        text-align: center;
+        width: 320px;
+        box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.2);
+        padding: 20px;
+    }
+</style>
