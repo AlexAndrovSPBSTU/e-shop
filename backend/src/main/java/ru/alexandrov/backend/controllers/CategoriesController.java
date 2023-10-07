4feb1b7949c6a154d59b1d7e5cd79b0a9d8ae828
Alexey Alexandrov <AleksandrovAlexey0929@yandex.ru>
2023-10-07 21:30:54 +0300

    feat(frontend): added categories' managing for admin

--- backend/src/main/java/ru/alexandrov/backend/controllers/CategoriesController.java
+++ backend/src/main/java/ru/alexandrov/backend/controllers/CategoriesController.java
@@ -1,10 +1,9 @@
 package ru.alexandrov.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.alexandrov.backend.models.Characteristic;
 import ru.alexandrov.backend.models.Product;
 import ru.alexandrov.backend.services.CategoryService;
@@ -35,5 +34,33 @@ public class CategoriesController {
     @GetMapping("/{category_id}/characteristics")
     public List<Characteristic> getCharacteristicsByCategory(@PathVariable("category_id") int id) {
         return categoryService.getCharacteristicsByCategoryId(id);
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<HttpStatus> createCategory(@RequestBody Category category,
+                                                     @RequestParam int parentId) {
+        categoryService.save(category, parentId);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{category_id}")
+    public ResponseEntity<HttpStatus> deleteCategory(@PathVariable("category_id") int id,
+                                                     @RequestParam int parentId) {
+        categoryService.delete(id, parentId);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PatchMapping("/insert/{category_id}")
+    public ResponseEntity<HttpStatus> insert(@PathVariable("category_id") int id,
+                                             @RequestParam Integer parentId) {
+        categoryService.insert(id, parentId);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PatchMapping("/rename/{category_id}")
+    public ResponseEntity<HttpStatus> rename(@PathVariable("category_id") int category_id,
+                                             @RequestParam("newName") String newName) {
+        categoryService.rename(category_id, newName);
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 }
--- backend/src/main/java/ru/alexandrov/backend/repositories/CategoryRepository.java
+++ backend/src/main/java/ru/alexandrov/backend/repositories/CategoryRepository.java
@@ -1,7 +1,10 @@
 package ru.alexandrov.backend.repositories;
 
+import org.hibernate.annotations.SQLDelete;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.alexandrov.backend.models.Category;
 
@@ -12,4 +15,19 @@ public interface CategoryRepository extends CrudRepository<Category, Integer> {
     @Query(value = "select * from category where category_id in (select child_id from category_parent_child where parent_id = 0)",
             nativeQuery = true)
     List<Category> getRootCategories();
+
+    @Modifying
+    @Query(value = "insert into category_parent_child values (:parentId,:childId)",
+            nativeQuery = true)
+    void insert(@Param("childId") int childId, @Param("parentId") int parentId);
+
+    @Modifying
+    @Query(value = "delete from category_parent_child where parent_id=:parentId and child_id=:childId",
+            nativeQuery = true)
+    void deleteChild(@Param("childId") int childId, @Param("parentId") int parentId);
+
+    @Modifying
+    @Query(value = "update category set name=:newName where category_id=:id",
+            nativeQuery = true)
+    void rename(@Param("id") int categoryId, @Param("newName") String newName);
 }
