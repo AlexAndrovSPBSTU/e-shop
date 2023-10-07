1778925e6c3db0ba9e94d96fcd31b25c9ce5f830
Alexey Alexandrov <AleksandrovAlexey0929@yandex.ru>
2023-10-07 21:32:04 +0300

    feat(frontend): added products' managing for admin

--- backend/src/main/java/ru/alexandrov/backend/services/ProductService.java
+++ backend/src/main/java/ru/alexandrov/backend/services/ProductService.java
@@ -2,11 +2,10 @@ package ru.alexandrov.backend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.alexandrov.backend.models.Category;
 import ru.alexandrov.backend.models.Product;
-import ru.alexandrov.backend.models.Property;
 import ru.alexandrov.backend.repositories.ProductRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +19,42 @@ public class ProductService {
 
     public Optional<Product> getProductById(int id) {
         return productRepository.findById(id);
+    }
+
+    public void save(Product product, int categoryId) {
+        Category category = new Category();
+        category.setId(categoryId);
+        product.setCategory(category);
+        productRepository.save(product);
+    }
+
+    public void delete(int id) {
+        productRepository.deleteById(id);
+    }
+
+    public void change(int id, String name, Float price, Integer amount,
+                       String description, Integer discount, Integer categoryId) {
+        Product product = productRepository.findById(id).get();
+        if (categoryId != null) {
+            Category category = new Category();
+            category.setId(categoryId);
+            product.setCategory(category);
+        }
+        if (name != null) {
+            product.setName(name);
+        }
+        if (price != null) {
+            product.setPrice(price);
+        }
+        if (amount != null) {
+            product.setAmount(amount);
+        }
+        if (discount != null) {
+            product.setDiscount(discount);
+        }
+        if (description != null) {
+            product.setDescription(description);
+        }
+        productRepository.save(product);
     }
 }
