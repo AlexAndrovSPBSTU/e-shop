0f8cb1a864ce2094a2f6f2b9673ff1d343a1cb17
Alexey Alexandrov <AleksandrovAlexey0929@yandex.ru>
2023-10-23 10:32:49 +0300

    feat(backend): changed db port at docker

--- backend/src/main/java/ru/alexandrov/backend/repositories/CategoryRepository.java
+++ backend/src/main/java/ru/alexandrov/backend/repositories/CategoryRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
-    @Query(value = "select * from category where category_id in (select child_id from category_parent_child where parent_id = 0)",
+    @Query(value = "select * from category where category_id in (select child_id from category_parent_child where parent_id = 1)",
             nativeQuery = true)
     List<Category> getRootCategories();
 
